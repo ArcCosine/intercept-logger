a071679510a68db2dd0d764739dda45d2e80f283
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-04-01 20:06:19 +0900

    [Update] Refactor.
    Refactor code.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-(() => {
-
+(async () => {
     "use strict";
 
     const puppeteer = require("puppeteer");
 
+    const browser = await puppeteer.launch();
 
-    puppeteer.launch().then(async browser => {
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
 
-        const page = await browser.newPage();
-        await page.setRequestInterception(true);
-
-        page.on("request", interceptedRequest => {
-            console.log(interceptedRequest.url);//debug
-            interceptedRequest.continue();
-        });
-
-        await page.goto("https://www.google.co.jp");
-        await browser.close();
-
+    page.on("request", async (interceptedRequest) => {
+        const url = await interceptedRequest.url();
+        console.log(url); //debug
+        interceptedRequest.continue();
     });
 
+    await page.goto("https://www.google.co.jp");
+    await browser.close();
 })();
